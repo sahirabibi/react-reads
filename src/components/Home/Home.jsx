cac75015bf03afe8723d220e08a14f183cd8e910
sahirabibi <sahirabibi96@gmail.com>
2021-09-17 19:19:01 -0400

    Styled home page with animations

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,14 +5,36 @@ import GenreList from '../GenreList/GenreList';
 // import { DataContext } from 'DataContext';
 import { Link, Route } from 'react-router-dom';
 import { useState, useEffect, useContext } from 'react';
+import { DataContext } from '../../DataContext';
 
 function Home(props) {
-    return (
-			<div className='main'>
+	const [display, setDisplay] = useState([]);
+	const { genres } = useContext(DataContext);
+
+	useEffect(() => {
+		// add all book image nested inside of genres to display
+		let newDisplay = genres
+			.map((genre) => genre.books.map((book) => book['book_image']))
+			.reduce((acc, item) => acc.concat(item), []);
+		setDisplay([...newDisplay]);
+	}, []);
+
+	return (
+		<div className='main'>
+			<div className='main-tag'>
 				<h2>Start Your Reading Journey Today</h2>
-				<Link to='/best-sellers'><button>Best Sellers List</button></Link>
+				<Link to='/best-sellers'>
+					<button>Best Sellers List</button>
+				</Link>
 			</div>
-		);
+
+			<div className='display'>
+				{display.splice(0, 17).map((book, idx) => (
+					<img className='display-book' id={`book-${idx}`} src={book}></img>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Home;
