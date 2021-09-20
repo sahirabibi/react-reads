7776f59d028a6e6f40c1f17131be81aec6f71534
sahirabibi <sahirabibi96@gmail.com>
2021-09-20 17:41:41 -0400

    Complete component that allows users to add a review for select books

--- src/components/MyReads/MyReviews.jsx
+++ src/components/MyReads/MyReviews.jsx
@@ -1,48 +1,49 @@
 import React from 'react';
-import {useEffect, useState, useContext} from 'react'
-import {useParams} from 'react-router-dom'
+import { useEffect, useState, useContext } from 'react';
+import { useParams } from 'react-router-dom';
 import { DataContext } from '../../DataContext';
 
-
 function MyReviews(props) {
-    // add a review for the selected book
-    // display book and beneath it display the review 
-    const { isbn } = useParams()
-    const {myReads, setMyReads} = useContext(DataContext)
-    const [myReview, setMyReview] = useState({
-        title: '',
-        review: '',
-        rating: ''
-    })
-    
-    // grab the details of this book from myReads 
-    const currentRead = myReads.filter(read => read.isbn_10[0] === isbn)
-    
-    function handleSubmit() {
+	// add a review for the selected book
+	// display book and beneath it display the review
+	const { isbn } = useParams();
+	const { myReads, setMyReads, MyReviews, setMyReviews } = useContext(DataContext);
+    const currentRead = myReads.filter((read) => read.isbn_10[0] === isbn);
+	const [myReview, setMyReview] = useState({
+		title: '',
+		review: '',
+		rating: null,
+	});
 
+	// grab the details of this book from myReads
+	
+
+	function handleSubmit(event) {
+        // update the current reviews with the new review 
+        event.preventDefault();
+        setMyReviews([...MyReviews, myReview])
     }
 
-    function handleChange() {
 
-    }
+	function handleChange(event) {
+		setMyReview({ ...myReview, [event.target.id]: event.target.value });
+	}
 
-    return (
-			<div>
-				{currentRead.map((item) => (
-					<h2>{item.title}</h2>
-				))}
-				<form onSubmit={handleSubmit}>
-					<label htmlFor='review-title' onChange={handleChange}>
-						Title
-					</label>
-					<input type='text' />
-					<label htmlFor='review' onChange={handleChange}>
-						Review
-					</label>
-					<input type='text' />
-				</form>
-			</div>
-		);
+	return (
+		<div>
+			{currentRead.map((item) => (
+				<h2>{item.title}</h2>
+			))}
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='review-title'>Title</label>
+				<input type='text' id='title' onChange={handleChange} />
+				<label htmlFor='review'>Review</label>
+				<input type='text' id='review' onChange={handleChange} />
+				<label htmlFor='rating'>Rating</label>
+				<input type='text' id='rating' onChange={handleChange} />
+			</form>
+		</div>
+	);
 }
 
 export default MyReviews;
