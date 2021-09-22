544e1c1558cbe773e9543159521d02d23a9a05dd
sahirabibi <sahirabibi96@gmail.com>
2021-09-22 12:14:46 -0400

    Styled search form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,27 +6,28 @@ import Search from './Search';
 function Header(props) {
 
     return (
-			<header>
-				<h1>Reads</h1>
-
-				<nav>
-					<ul>
-						<Link to='/'>
-							<li>Home</li>
-						</Link>
-						<Link to='/best-sellers'>
-							<li>Featured List</li>
-						</Link>
-						<Link to='/my-reads'>
-							<li>My Reads</li>
-						</Link>
-						<Link to='/about'>
-							<li>About</li>
-						</Link>
-					</ul>
-					<Search />
-				</nav>
-			</header>
+			<div className='main-header'>
+				<header>
+					<h1>Reads</h1>
+					<nav>
+						<ul>
+							<Link to='/'>
+								<li>Home</li>
+							</Link>
+							<Link to='/best-sellers'>
+								<li>Featured List</li>
+							</Link>
+							<Link to='/my-reads'>
+								<li>My Reads</li>
+							</Link>
+							<Link to='/about'>
+								<li>About</li>
+							</Link>
+						</ul>
+					</nav>
+				</header>
+				<Search/>
+			</div>
 		);
 }
 
