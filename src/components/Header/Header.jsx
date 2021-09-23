696a53185a5c14fb0888270da4747fa2e3d384df
sahirabibi <sahirabibi96@gmail.com>
2021-09-22 23:26:16 -0400

    Move search.jsx to search folder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-
-
-import Search from './Search';
+import Search from '../Search/Search';
 
 function Header(props) {
 
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -72,4 +72,3 @@ function Search(props) {
 }
 
 export default Search;
-
